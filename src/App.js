c91c0f166526c20fa9200667cc697eef597e16f8
Athira K S <athiraks@Athiras-MacBook-Air.local>
2024-05-03 14:17:39 +0530

    blog card design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import IllustrationImage from "./assets/images/illustration-article.svg";
+import Avatar from "./assets/images/image-avatar.webp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="blog-preview-card">
+      <img className="image" src={IllustrationImage} alt="illustration"/>
+      <div className="badge">Learning</div>
+      <div className="published-on">Published 21 Dec 2023</div>
+      <h1 className="heading">HTML & CSS foundations</h1>
+      <p className="description">
+        These languages are the backbone of every website, defining structure,
+        content, and presentation.
+      </p>
+      <div className="avatar-container">
+        <div className="avatar">
+          <img src={Avatar} alt="avatar"/>
+        </div>Greg Hooper
+      </div>
+      
     </div>
   );
 }
